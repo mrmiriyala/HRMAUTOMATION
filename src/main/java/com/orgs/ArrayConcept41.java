a029563a722bd5f1dba24c7afa20ec3be3def253
mrmiriyala <mkravi2909@gmail.com>
2024-02-28 15:59:26 +0000

    APT

---
+++ src/main/java/com/orgs/ArrayConcept41.java
@@ -0,0 +1,34 @@
+package com.orgs;
+
+public class ArrayConcept41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+int a[]= {10,11,13,16,17,19,15,22,24,27,29};
+System.out.println("The length of Array:" +a.length);
+//printing each anf every value in the array
+for(int i=0;i<a.length;i++)
+{
+	System.out.println(a[i]);
+}
+int b[] = {5,10,15,20,25,30};
+System.out.println("Array B lenth is " +b.length);
+System.out.println(b[0]);
+System.out.println(b[1]);
+System.out.println(b[2]);
+System.out.println(b[3]);
+System.out.println(b[4]);
+System.out.println(b[5]);
+
+System.out.println("Printing the array values using Loops");
+
+for(int j=0;j<=b.length;j++)
+{
+	System.out.println(b[j]);
+}
+
+	}
+
+}
