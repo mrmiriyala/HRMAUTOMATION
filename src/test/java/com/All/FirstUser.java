b7ff77e4f5dbd4ccc778b153ae9683086a093580
mrmiriyala <mkravi2909@gmail.com>
2024-02-28 16:07:32 +0000

    QUT

--- src/test/java/com/All/FirstUser.java
+++ src/test/java/com/All/FirstUser.java
@@ -15,7 +15,8 @@ public class FirstUser {
 		driver.manage().window().maximize();
 		String exp="https://m.facebook.com/login/?locale=en";
 		String actual = driver.getCurrentUrl();
-		Assert.assertTrue(exp.equals(actual));
+		Assert.assertFalse(exp.equals(actual));
+		
   given()
   .when()
  .get("https://reqres.in/api/users?page=2")
@@ -23,4 +24,22 @@ public class FirstUser {
  .statusCode(200)
  .log().all();
 }
+	@Test(priority=1)
+	public void seconduser()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://m.facebook.com/login/?locale=en_GB");
+		driver.manage().window().maximize();
+		String exp="https://m.facebook.com/login/?locale=en";
+		String actual = driver.getCurrentUrl();
+		Assert.assertFalse(exp.equals(actual));
+		
+  given()
+  .when()
+ .get("https://reqres.in/api/users?page=2")
+ .then()
+ .statusCode(200)
+ .log().all();	
+	}
 }
